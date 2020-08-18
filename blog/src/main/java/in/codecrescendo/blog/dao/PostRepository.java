a1ab4bc37dd7d8a5dff235d34f5b563e4e18be3a
Namrata Khot <namratakhot94@gmail.com>
2020-08-18 11:50:33 +0530

    Update PostRepository.java
    user form changes

--- blog/src/main/java/in/codecrescendo/blog/dao/PostRepository.java
+++ blog/src/main/java/in/codecrescendo/blog/dao/PostRepository.java
@@ -1,10 +1,9 @@
 package in.codecrescendo.blog.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.codecrescendo.blog.entity.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import in.codecrescendo.blog.entity.Post;
 
 
 @Repository
